5f0116e064942ffaec0dc2b1bda2a8a968f094c2
Armando Ugalde <armandougalde@gmail.com>
2019-08-29 23:09:27 -0500

    dd

---
+++ Shop/Shop.Web/Data/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.Web.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Web.Data
+{
+    public class DataContext : DbContext
+    {
+
+        
+        public DataContext(DbContextOptions<DataContext>options) :base(options)
+        {
+
+        }
+
+
+        public DbSet<Product> Products { get; set; }
+
+
+
+
+    }
+}
---
+++ Shop/Shop.Web/Data/Entities/Product.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Web.Data.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Image")]
+        public string ImageUrl { get; set; }
+
+        [Display(Name = "Last Purchase")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime LastPurchase { get; set; }
+
+        [Display(Name = "Last Sale")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime LastSale { get; set; }
+
+        [Display(Name = "Is Availabe?")]
+        public bool IsAvailabe { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = false)]
+        public double Stock { get; set; }
+
+    }
+}
